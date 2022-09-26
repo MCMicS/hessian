3357c69fb082728ff7f1dd0f19f93a446bdadc03
msuhr <msuhr@vjoon.com>
2022-09-26 15:27:25 +0200

    add cyclic test

--- test/com/caucho/hessian/io/SerializerDeserializerTest.java
+++ test/com/caucho/hessian/io/SerializerDeserializerTest.java
@@ -51,8 +51,50 @@ public class SerializerDeserializerTest {
         Assert.assertEquals(testUser2.getLastSeen().getTimeInMillis(), readTestUsers[1].getLastSeen().getTimeInMillis());
     }
 
+    @Test
+    public void testSerializeDeserializeCalendarWithCyclicReference() throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        Hessian2Output out = new Hessian2Output(bos);
+        //out.setUnshared(true);
+
+        out.startMessage();
+        Calendar today = Calendar.getInstance(Locale.ENGLISH);
+        TestUser testUser1 = new TestUser("User1", today);
+        TestUser testUser2 = new TestUser("User2", today);
+        testUser1.setFriend(testUser2);
+        testUser2.setFriend(testUser1);
+        TestUser[] testUsers = new TestUser[] {testUser1, testUser2};
+        out.writeObject(testUsers);
+
+        out.completeMessage();
+        out.close();
+
+        byte[] data = bos.toByteArray();
+
+        // Deserialize
+        ByteArrayInputStream bin = new ByteArrayInputStream(data);
+        Hessian2Input in = new Hessian2Input(bin);
+
+        in.startMessage();
+        final Object readObject = in.readObject(TestUser[].class);
+
+        in.completeMessage();
+        in.close();
+        bin.close();
+        Assert.assertTrue(readObject instanceof TestUser[]);
+        final TestUser[] readTestUsers = (TestUser[]) readObject;
+        Assert.assertEquals(2, readTestUsers.length);
+        Assert.assertEquals(testUser1.name, readTestUsers[0].getName());
+        Assert.assertThat(readTestUsers[0].getLastSeen(), Matchers.instanceOf(Calendar.class));
+        Assert.assertEquals(testUser1.getLastSeen().getTimeInMillis(), readTestUsers[0].getLastSeen().getTimeInMillis());
+        Assert.assertEquals(testUser2.name, readTestUsers[1].getName());
+        Assert.assertThat(readTestUsers[1].getLastSeen(), Matchers.instanceOf(Calendar.class));
+        Assert.assertEquals(testUser2.getLastSeen().getTimeInMillis(), readTestUsers[1].getLastSeen().getTimeInMillis());
+    }
+
     private static class TestUser implements java.io.Serializable {
         private final String name;
+        private TestUser friend;
 
         public String getName() {
             return name;
@@ -67,6 +109,14 @@ public class SerializerDeserializerTest {
         private TestUser(String name, Calendar lastSeen) {
             this.name = name;
             this.lastSeen = lastSeen;
+        }
+
+        public void setFriend(final TestUser friend) {
+            this.friend = friend;
+        }
+
+        public TestUser getFriend() {
+            return friend;
         }
     }
 }
